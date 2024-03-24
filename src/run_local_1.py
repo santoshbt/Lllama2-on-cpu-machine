d2a8e71f1cb076fa4a408856ba2a1b6a2592f92a
llssbb <bt.santosh01@gmail.com>
2024-03-24 16:28:04 +0000

    inference added

--- src/run_local_1.py
+++ src/run_local_1.py
@@ -6,9 +6,10 @@ from src.helper import *
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
-instruction = "Convert the following text from English to Hindi: \n\n {text}"
+# instruction = "Convert the following text from English to Hindi: \n\n {text}"
+instruction = "Give a proper summary of the book: \n\n {text}"
 
-SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + E_SYS
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
 
 
@@ -22,4 +23,4 @@ llm = CTransformers(model='model/llama-2-7b-chat.ggmlv3.q4_0.bin',
 
 LLM_Chain=LLMChain(prompt=prompt, llm=llm)
 
-print(LLM_Chain.run("How are you?"))
+print(LLM_Chain.run("Harry Potter"))
